cca9cf936e4cb01ac96ead874601a64273f929d2
Darrenni97 <Darrenni_@hotmail.com>
2019-09-18 21:23:23 +0000

    created array for each letter & if statement to ignore white space

---
+++ letterPositions.js
@@ -0,0 +1,16 @@
+const letterPositions = function(sentence) {
+  const results = {};
+ 
+  const split = sentence.split('') //creates array for every letter
+
+for (let letter of split) {
+  if (letter !== ' ') { // ignores white spaces
+    // results.letter +=
+  }
+}
+  // logic to update results here
+
+  return results;
+}; 
+
+console.log(letterPositions("lighthouse in the house"));
