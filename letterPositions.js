68870342504e47881413814faf3fbef77c78e543
Darrenni97 <Darrenni_@hotmail.com>
2019-09-18 22:06:55 +0000

    function to return positions of letters

--- letterPositions.js
+++ letterPositions.js
@@ -1,16 +1,38 @@
 const letterPositions = function(sentence) {
   const results = {};
- 
-  const split = sentence.split('') //creates array for every letter
 
-for (let letter of split) {
-  if (letter !== ' ') { // ignores white spaces
-    // results.letter +=
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i];
+    if (results[letter]) {
+      results[letter].push(i);
+    } else {
+      results[letter] = [i]
+    }
   }
-}
+  delete results[' '];
   // logic to update results here
-
   return results;
 }; 
 
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed:${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !=== ${expected}`);
+  }
+}
+
 console.log(letterPositions("lighthouse in the house"));
+assertArraysEqual(letterPositions("hello").e, [1]);
