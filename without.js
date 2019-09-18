9aaccfd38e8b8540af2cb18aa27a949f296d3a23
Darrenni97 <Darrenni_@hotmail.com>
2019-09-18 01:58:46 +0000

    created function that compares 2 arrays and filters out the second from first array

---
+++ without.js
@@ -0,0 +1,42 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed:[${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: [${actual}] !=== [${expected}]`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let newArr = [];
+
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] !== itemsToRemove[j]) {
+        newArr.push(source[i]);
+      }
+    }
+  }
+  return newArr;
+};
+
+
+
+// TEST
+const words = ["hello", "world", "lighthouse"];
+without(["hello", "world", "lighthouse"], ["lighthouse"])); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
