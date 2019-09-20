c8d13c0ea2b4a43f350b7d0c97fb11904874a1fc
Darrenni97 <Darrenni_@hotmail.com>
2019-09-19 18:58:07 -0700

    implemented assertArraysEqual to do assertion tests

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,24 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed:${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !=== ${expected}`);
+  }
+}
+
+
 const takeUntil = function(array, callback) {
   results = []
   for (let item of array) {
@@ -24,3 +45,5 @@ const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Re
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
