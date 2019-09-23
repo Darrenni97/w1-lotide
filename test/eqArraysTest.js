27504e0e83991c1fa34d011921e090f58140dab2
Darrenni97 <Darrenni_@hotmail.com>
2019-09-23 22:16:04 +0000

    refactored with mocha and chai, pair programming with Anthony

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,5 +1,13 @@
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
+
+describe("#eqArrays", () => {
+
+  it("returns true when passed [1, 2, 3] and [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+});
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
