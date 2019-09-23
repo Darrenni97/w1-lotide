2a156726f922d69ee991c05bae7fa92638c633e4
Darrenni97 <Darrenni_@hotmail.com>
2019-09-23 22:05:24 +0000

    installed mocha and chai, refactor assertArraysEqualTest.js, pair programming with Anthony

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,15 @@
-
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual')
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); 
-assertArraysEqual([1, 2, 3], [3, 2, 1])
+describe('#assertArraysEqual', () => {
+  it('returns true [1, 2, 3] for [1, 2, 3]', () => {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+  });
+
+  it('returns false [1, 2, 3] for [3, 2, 1]', () => {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [3, 2, 1]));
+  });
+
+});
+// assertArraysEqual([1, 2, 3], [1, 2, 3]); 
+// assertArraysEqual([1, 2, 3], [3, 2, 1])
--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,15 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
-assertEqual(head([4,5,6]), 4);
-assertEqual(head(["hello", "lighthouse", "labs"],"hello"));
+const assert = require('chai').assert;
+const head   = require('../head');
+// assertEqual(head([4,5,6]), 4);
+// assertEqual(head(["hello", "lighthouse", "labs"],"hello"));
+
+describe("#head", () => {
+
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['4']", () => {
+    assert.strictEqual(head(['5']), '4'); 
+  });
+});
