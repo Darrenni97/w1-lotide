5c9f8a4d0a80b652de1803089a210861fd34c0e1
Darrenni97 <Darrenni_@hotmail.com>
2019-09-23 22:12:04 +0000

    refactored with mocha and chai, pair programming with Anthony

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,4 +1,17 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
-assertEqual("Lighthouse labs","bootcamp");
-assertEqual(1,1);
+describe('#assertEqual', () => {
+
+  it('returns false because "Lighthouse labs !== "bootcamp"', () => {
+    assert.strictEqual(assertEqual("Lighthouse labs","bootcamp"));
+  });
+
+  it('returns true because 1 === 1', () => {
+    assert.strictEqual(assertEqual(1, 1));
+  });
+
+})
+
+// assertEqual("Lighthouse labs","bootcamp");
+// assertEqual(1,1);
