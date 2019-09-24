079879e4b3398264edb28e6e22238547c0bfd4e7
Darrenni97 <Darrenni_@hotmail.com>
2019-09-24 01:20:55 +0000

    fix error

--- without.js
+++ without.js
@@ -37,7 +37,7 @@ const without = function(source, itemsToRemove) {
 
 // TEST
 const words = ["hello", "world", "lighthouse"];
-without(["hello", "world", "lighthouse"], ["lighthouse"])); // no need to capture return value for this test case
+without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
